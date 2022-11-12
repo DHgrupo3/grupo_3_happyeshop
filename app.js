4e49441960b394f441794cbfbb4a37c3710f18c9
rodro2 <rodrigovela22@gmail.com>
2022-11-12 10:46:27 -0300

    partialFooter

--- app.js
+++ app.js
@@ -10,38 +10,40 @@ const path = require('path');
 //usando recursos estáticos
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
+
 //Al objeto app le pedidos el metodo listen, que se encargará de levantar el servidor. Parámetros: PUERTO | FUNCIÓN (Callback)
 app.listen(3000, () => console.log ("Servidor corriendo"));
 
 //Al objeto app le pedimos dos parámetros URL | CallBack (req, res) : Req (Toda la info de la peticón que llego) y Res (Todas la propiedades y metodos de la respuesta que vamos a enviar)
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html");
+    res.render(__dirname + "/views/index");
 })
 
 app.get("/register", (req,res) => {
-    res.sendFile(__dirname + "/views/register.html");
+    res.render(__dirname + "/views/register");
 })
 
 app.get("/login", (req,res) => {
-    res.sendFile(__dirname + "/views/login.html");
+    res.render(__dirname + "/views/login");
 })
 
 app.get("/products", (req,res) => {
-    res.sendFile(__dirname + "/views/productList.html");
+    res.render(__dirname + "/views/productList");
 })
 
 app.get("/cart", (req,res) => {
-    res.sendFile(__dirname + "/views/productCart.html");
+    res.render(__dirname + "/views/productCart");
 })
 
 app.get("/who", (req,res) => {
-    res.sendFile(__dirname + "/views/who.html");
+    res.render(__dirname + "/views/who");
 })
 
 app.get("/terminos", (req,res) => {
-    res.sendFile(__dirname + "/views/terminos.html");
+    res.render(__dirname + "/views/terminos");
 })
 
 app.get("/contactanos", (req,res) => {
-    res.sendFile(__dirname + "/views/contactanos.html");
+    res.render(__dirname + "/views/contactanos");
 })
