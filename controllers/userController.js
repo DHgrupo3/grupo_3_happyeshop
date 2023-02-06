7643f70ba9d8fe6a743fdfff7f5da16ac340a3df
emiliobuzzi <enbuzzi@gmail.com>
2023-02-06 17:49:46 -0300

    Spring 5 Validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const {json} = require('express');
 //const { all } = require('../routes/users');
 
+const { validationResult } = require('express-validator');
+
 const User = require('../src/models/user.js');
 
 const controller = {
@@ -30,15 +32,15 @@ const controller = {
         //Reviso que es lo que recibo
         console.log(req.body, req.file);
 
-        ////  No esta definido  validationResult y es por eso que da error
-        // const resultValidation = validationResult(req);
+        //  No esta definido  validationResult y es por eso que da error
+        const resultValidation = validationResult(req);
 		
-		// if (resultValidation.errors.length > 0) {
-		// 	return res.render('./users/register', {
-		// 		errors: resultValidation.mapped(),
-		// 		oldData: req.body
-		// 	});
-		// }
+        if (resultValidation.errors.length > 0) {
+			return res.render('./users/register', {
+				errors: resultValidation.mapped(),
+				oldData: req.body
+			});
+		}
 
         let userToCreate = {
             ...req.body,
@@ -47,7 +49,8 @@ const controller = {
 
         User.create(userToCreate);
 
-		return res.send('Ok, las validaciones se pasaron y no tienes errores');
+		// //return res.send('Ok, las validaciones se pasaron y no tienes errores');
+        // return res.render ('./users/register');
 	},
 
 }
